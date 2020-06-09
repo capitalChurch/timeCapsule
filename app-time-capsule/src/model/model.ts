006c0613852d933ca2c7efe7b859e025ba81413e
Kevyn Klava <kevynsax@gmail.com>
2020-06-09 12:50:46 -0300

    Correct address of backend

--- app-time-capsule/src/model/model.ts
+++ app-time-capsule/src/model/model.ts
@@ -4,14 +4,12 @@ import axios from 'axios';
 const development = 'development';
 
 export const saveMessage = (obj: Message, callBack: () => void, callBackError: (err: any) => void): void => {
-    const urlProduction = 'http://cartaparaofuturo.azurewebsites.net/';
-    const urlDevelopment = 'http://localhost:44396/';
-    const urlBase = process.env.NODE_ENV === development ? urlDevelopment : urlProduction;
-    const url = `${urlProduction}api`;
+    const urlProduction = 'https://159.65.102.222/time-capsule';
+    const urlDevelopment = 'http://localhost:44396/api';
+    const url = process.env.NODE_ENV === development ? urlDevelopment : urlProduction;
     const urlMessage = `${url}/TimeCapsule/`;
 
     axios.post(urlMessage, obj)
         .then(callBack)
         .catch(callBackError);
 };
-
